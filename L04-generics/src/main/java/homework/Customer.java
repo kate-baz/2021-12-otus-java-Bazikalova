863f08296bc23a2138adbe6636f23d3e759656d6
Ekaterina <e.bazikalova@gmail.com>
2022-02-20 02:22:40 +1100

    ДЗ №2. Применение коллекций

---
+++ L04-generics/src/main/java/homework/Customer.java
@@ -0,0 +1,66 @@
+package homework;
+
+public final class Customer implements Comparable<Customer> {
+    private final long id;
+    private String name;
+    private Long scores;
+
+    //todo: 1. в этом классе надо исправить ошибки
+
+    public Customer(long id, String name, long scores) {
+        this.id = id;
+        this.name = name;
+        this.scores = scores;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getScores() {
+        return scores;
+    }
+
+    public void setScores(long scores) {
+        this.scores = scores;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", scores=" + scores +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        return id == customer.id;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public int compareTo(Customer o) {
+        return scores.compareTo(o.scores);
+    }
+}
