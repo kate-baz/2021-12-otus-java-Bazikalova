7c716170c0c26ee25551030d83d18aa4faf9d442
Ekaterina <e.bazikalova@gmail.com>
2022-05-13 22:17:42 +1100

    ДЗ №4. Определение нужного размера хипа

---
+++ L08-gc/homework/src/main/java/ru/calculator/CalcDemo.java
@@ -0,0 +1,53 @@
+package ru.calculator;
+
+
+/*
+-Xms256m
+-Xmx256m
+-XX:+HeapDumpOnOutOfMemoryError
+-XX:HeapDumpPath=./logs/heapdump.hprof
+-XX:+UseG1GC
+-Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level:filecount=5,filesize=10m
+*/
+
+/*
+Before optimisation:
+spend msec:13558, sec:13 - 1g
+spend msec:14294, sec:14 - 2g
+spend msec:13096, sec:13 - 3g
+spend msec:14570, sec:14 - 4g
+spend msec:13707, sec:13 - 5g
+
+After optimisation:
+spend msec:2493, sec:2 - 64m
+spend msec:2244, sec:2 - 128m
+spend msec:2297, sec:2 - 256m
+ */
+
+import java.time.LocalDateTime;
+
+public class CalcDemo {
+    public static void main(String[] args) {
+        long counter = 100_000_000;
+        var summator = new Summator();
+        long startTime = System.currentTimeMillis();
+
+        var data = new Data();
+        for (var idx = 0; idx < counter; idx++) {
+            data.setValue(idx);
+            summator.calc(data);
+
+            if (idx % 10_000_000 == 0) {
+                System.out.println(LocalDateTime.now() + " current idx:" + idx);
+            }
+        }
+
+        long delta = System.currentTimeMillis() - startTime;
+        System.out.println(summator.getPrevValue());
+        System.out.println(summator.getPrevPrevValue());
+        System.out.println(summator.getSumLastThreeValues());
+        System.out.println(summator.getSomeValue());
+        System.out.println(summator.getSum());
+        System.out.println("spend msec:" + delta + ", sec:" + (delta / 1000));
+    }
+}
